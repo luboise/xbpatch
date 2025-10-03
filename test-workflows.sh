ac0bb8710957ca43965ddcc527c2fa12503498b1
luboise <lucas2@live.com.au>
2025-10-03 20:05:38 +1000

    Fixed Windows CI

--- test-workflows.sh
+++ test-workflows.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-act -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:rust-latest -P windows-latest=mcr.microsoft.com/devcontainers/rust:latest
+act -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:rust-latest --matrix os:ubuntu-latest && 
+
+# Keep these separated, act can get confused when running these at the same time
+act -P windows-latest=mcr.microsoft.com/devcontainers/rust:latest --matrix os:windows-latest
