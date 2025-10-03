46dec361e8a63db22c48af662ba8d092c56a8c21
luboise <lucas2@live.com.au>
2025-10-03 19:38:36 +1000

    Added Windows CI

--- test-workflows.sh
+++ test-workflows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-act -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:rust-latest
+act -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:rust-latest -P windows-latest=mcr.microsoft.com/devcontainers/rust:latest
